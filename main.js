6819c9fc56bf5886d67d5db21802e1704004261b
useralbert <al510new@gmail.com>
2019-06-09 12:55:52 +0800

    revised message

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const express = require('express')
 const mysql = require('mysql')
 const bodyParser = require('body-parser');
 const request = require('request');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config.json');
 

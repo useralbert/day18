c66074f39719f115c1023ac7171571f01a15f157
useralbert <al510new@gmail.com>
2019-06-10 14:18:36 +0800

    revised message

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ const cookieParser = require('cookie-parser');
 
 const config = require('./config.json');
 
-const SQL_SELECT_EMPLOYEE = "select * from employees limit ? offset ?";
-const SQL_SELECT_EMPLOYEE_BY_EMPNO = "select * from employees where emp_no = ?";
+const SQL_SELECT_BGG_GAME = "select * from employees limit ? offset ?";
+const SQL_SELECT_BGG_GAME_BY_NAME = "select name from game";
 
-const empPool = mysql.createPool(config.employees)
+//const Pool = mysql.createPool(config.bgg)
+const pool = mysql.createPool(require('./config.json'));
 
 const PORT = parseInt(process.argv[2] || process.env.APP_PORT || 3000);
 
@@ -20,7 +21,97 @@ app.engine('hbs', hbs())
 app.set('view engine', 'hbs')
 app.set('views', __dirname + '/views');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/search', (req, resp) => {
+    const q = req.body.q;
+    //const cart = JSON.parse(req.body.cart);
+    pool.getConnection((err, conn) => {
+        if (err) {
+            resp.status(500);
+            resp.type('text/plain');
+            resp.send(err);
+            return;
+        }
+        //Perform our query
+        //conn.query(SQL_SELECT_FILM, //(err, result) => {
+        //conn.query(SQL_SELECT_FILM_WHERE, 
+        conn.query(SQL_SELECT_BGG_GAME_BY_NAME,
+            [ ],
+            (err, result) => {
+            //Release the connection
+                conn.release();
+                console.info(result);
+                console.info(typeof result[1]);
+                if (err) {
+                    resp.status(500);
+                    resp.type('text/plain');
+                    resp.send(err);
+                    return;
+                }
+                resp.status(200);
+                resp.type('text/html');
+                resp.render('games', { 
+                    name: name, 
+                    result: result,
+                    q: q,
+                    noResult: result.length <= 0,
+                    layout: false 
+                });
+            }
+        )
+    });
+})
+
+ /*   resp.status(200)
+    resp.type('text/html')
+    resp.render('search', { 
+        name: name,
+        cart: JSON.stringify(cart),
+        items: cart,
+        layout: false
+    }) */
+
+
+
+app.get('/search', (req, resp) => {
+    const q = req.query.q;
+    pool.getConnection((err, conn) => {
+        if (err) {
+            resp.status(500);
+            resp.type('text/plain');
+            resp.send(err);
+            return;
+        }
+        //Perform our query
+        //conn.query(SQL_SELECT_FILM, //(err, result) => {
+        //conn.query(SQL_SELECT_FILM_WHERE, 
+        conn.query(SQL_SELECT_BGG_GAME_BY_NAME,
+            [ ],
+            (err, result) => {
+            //Release the connection
+                conn.release();
+                console.info(result);
+                console.info(typeof result[1]);
+                if (err) {
+                    resp.status(500);
+                    resp.type('text/plain');
+                    resp.send(err);
+                    return;
+                }
+                resp.status(200);
+                resp.type('text/html');
+                resp.render('games', { 
+                    name: name, 
+                    result: result,
+                    q: q,
+                    noResult: result.length <= 0,
+                    layout: false 
+                });
+            }
+        )
+    });
+})
 
 
 
@@ -40,3 +131,7 @@ app.use(bodyParser.urlencoded());
 
 
 app.get(/.*/, express.static(__dirname + '/public'));
+
+app.listen(PORT, () => {
+	console.info(`Application started at ${new Date()} on port ${PORT}`);
+});
